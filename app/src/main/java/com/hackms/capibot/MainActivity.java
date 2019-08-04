54c6be3dee258324aafce04545e1e808b8d53363
Cesar Bassani <cesarbassani@Cesars-MacBook-Pro.local>
2019-08-04 10:37:03 -0400

    configurando requisicoes no DialogFlow

--- app/src/main/java/com/hackms/capibot/MainActivity.java
+++ app/src/main/java/com/hackms/capibot/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hackms.capibot;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText consultaEdText;
     private TextView resultadoTxtView;
+
+    private AIService aiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +32,52 @@ public class MainActivity extends AppCompatActivity {
         resultadoTxtView = findViewById(R.id.resultado_text);
 
         final AIConfiguration config = new AIConfiguration(CLIENT_ACCESS_TOKEN,
-                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.SupportedLanguages.PortugueseBrazil,
                 AIConfiguration.RecognitionEngine.System);
+
+        aiService = AIService.getService(this, config);
     }
 
     public void consultar(View view) {
-        resultadoTxtView.setText(consultaEdText.getText().toString());
+
+        if (consultaEdText.getText().toString().trim().equals("")) {
+            Toast.makeText(this, "Digite algo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        consultaEdText.setText("");
+
+        final AIRequest aiRequest = new AIRequest();
+        aiRequest.setQuery(consultaEdText.getText().toString());
+
+        new AsyncTask<AIRequest, Void, AIResponse>() {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                resultadoTxtView.setText("Consultado Dialogflow");
+            }
+
+            @Override
+         protected AIResponse doInBackground(AIRequest... requests) {
+             final AIRequest request = requests[0];
+             try {
+                 final AIResponse response = aiService.textRequest(aiRequest);
+                 return response;
+             } catch (AIServiceException e) {
+
+             }
+             return null;
+         }
+         @Override
+            protected void onPostExecute(AIResponse aiResponse) {
+             if (aiResponse == null) {
+                 resultadoTxtView.setText("Ocorreu um erro!");
+             } else {
+                 resultadoTxtView.setText(aiResponse.getResult().getFulfillment().getSpeech());
+             }
+         }
+        }.execute(aiRequest);
+
     }
 }
